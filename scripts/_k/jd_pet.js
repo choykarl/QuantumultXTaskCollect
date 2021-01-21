c333dbe4654f6a87c2fcdc86ccfe4e3112f5a857
karl <253440030@qq.com>
2021-01-21 23:26:39 +0800

    更新投食

--- scripts/_k/jd_pet.js
+++ scripts/_k/jd_pet.js
@@ -128,7 +128,7 @@ async function jdPet() {
       await slaveHelp();//助力好友
       await masterHelpInit();//获取助力的信息
       await doTask();//做日常任务
-      await feedPetsAgain();//再次投食
+      // await feedPetsAgain();//再次投食
       await energyCollect();//收集好感度
       await showMsg();
       console.log('全部任务完成, 如果帮助到您可以点下🌟STAR鼓励我一下, 明天见~');
